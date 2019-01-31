4d3e95b5d4f3acb4fa4fd568eb6b78e30852426a
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-31 13:53:06 +0800

    Header（导航栏）组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-
+    <Header/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style>
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="logo">
+    <ul>
+      <li>
+        <a href="#">首页</a>
+      </li>
+      <li>
+        <a href="#">新手入门</a>
+      </li>
+      <li>
+        <a href="#">API</a>
+      </li>
+      <li>
+        <a href="#">关于</a>
+      </li>
+      <li>
+        <a href="#">注册</a>
+      </li>
+      <li>
+        <a href="#">登录</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style  scoped>
+.header {
+  display: flex;
+  background-color: #444;
+  height: 50px;
+  justify-content: space-between;
+  align-items: center;
+}
+img {
+  max-width: 120px;
+  margin-left: 65px;
+}
+ul {
+  list-style: none;
+  margin-right: 80px;
+}
+li {
+  display: inline-block;
+  margin-left: 30px;
+}
+a {
+  text-decoration: none;
+  color:#ccc;
+  font-size: 13px;
+}
+a:hover{
+  color: #fff
+}
+</style>
