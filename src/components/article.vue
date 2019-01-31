a4fb358d37c054b641b596fad6a9f6e0d0a5a63a
FanKaiqiang <fankaiqiang001@gmail.com>
2019-02-01 03:53:15 +0800

    文章评论部分

--- src/components/article.vue
+++ src/components/article.vue
@@ -20,10 +20,19 @@
     </header>
     <article v-html="word"></article>
     <div class="comment">
+      <div class="topbar">{{post.reply_count}} 回复</div>
       <ul>
-        <li></li>
+        <li v-for="(reply,index) in post.replies" :key="index" :id="reply.id">
+          <div><img class="avatar" :src="reply.author.avatar_url" alt="avatar"></div>
+          <div class="content">
+            <div class="authorMessage"><span>{{reply.author.loginname}} </span><a :href="`#${reply.id}`">{{index+1}}楼•{{reply.create_at|formatDate}}</a></div>
+            <div class="replyMessage" v-html="reply.content"></div>
+          </div>
+          <div class="good">赞{{reply.ups.length}}</div>
+        </li>
       </ul>
     </div>
+    <footer></footer>
   </div>
 </template>
 <script>
@@ -56,7 +65,7 @@ export default {
       this.$axios
         .get(`https://cnodejs.org/api/v1/topic/${this.$route.params.id}`)
         .then(res => {
-          console.dir(res.data.data.content.replace(/\n/g,"<br/>"));
+          console.dir(res.data.data);
           this.post = res.data.data;
         });
     }
@@ -66,7 +75,7 @@ export default {
   }
 };
 </script>
-<style>
+<style scope>
 /* @import url('../assets/markdown-github.css');  */
 .comment > ul {
   list-style: none;
@@ -75,9 +84,13 @@ a {
   text-decoration: none;
 }
 .article {
-  background-color: #fff;
+  background-color: #e1e1e1;
   display: flex;
   flex-direction: column;
+  margin-top: 1em;
+}
+header{
+  background-color: #fff;
 }
 .titleWrapper {
   margin: 1em 0;
@@ -112,11 +125,12 @@ a {
   content: "• ";
 }
 article{
+  background-color: #fff;
   padding: 1em 1.5em;
   font-size: 15px;
   color: #333;
 }
-article img{
+img{
   max-width: 100%
 }
 article ul{
@@ -134,5 +148,42 @@ article  li::before {
   content: "•";
   margin-right: .5em;
   font-size: 18px;
+}
+.comment{
+  background-color: #fff;
+  margin-top: 1em;
+  border-radius: 4px;
+  position: relative;
+}
+.comment li{
+  display: flex;
+  border-bottom: 1px solid #f0f0f0;
+  min-height: 80px;
+  padding: .8em;
+}
+.content{
+  margin-left: .8em;
+}
+.authorMessage{
+  font-size: 12px;
+}
+.replyMessage{
+  margin: .5em;
+}
+.topbar{
+  height: 40px;
+  background-color: #f6f6f6;
+  color: #444;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  padding: .5em;
+}
+.avatar{
+  width: 30px;
+}
+.good{
+  position: absolute;
+  right: 1em;
 }
 </style>
